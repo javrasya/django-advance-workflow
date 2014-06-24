87d235d98ba2e76775a28ffebec4d8939a383ba9
Ahmet DAL <ceahmetdal@gmail.com>
2014-06-24 18:38:46 +0300

    Approve and reject button texts are parametrized from template .

--- daw/templatetags/process_buttons.py
+++ daw/templatetags/process_buttons.py
@@ -1,9 +1,7 @@
 from django import template
-from django.contrib.admin.templatetags.admin_modify import submit_row
 from django.contrib.contenttypes.models import ContentType
-from daw.service.approvementservice import ApprovementService
 
-from daw.service.objectservice import ObjectService
+from daw.service.approvementservice import ApprovementService
 
 
 register = template.Library()
@@ -14,12 +12,14 @@ DAW_PROCESS_BUTTONS_TEMPLATE = 'process_buttons.html'
 
 
 @register.inclusion_tag(DAW_PROCESS_BUTTONS_TEMPLATE, takes_context=True)
-def daw_process_buttons(context, obj_pk, cls, state_field):
+def daw_process_buttons(context, obj_pk, cls, state_field, approve_button_text=None, reject_button_text=None):
     cls = ContentType.objects.get_for_model(cls).model_class()
     obj = cls.objects.get(pk=obj_pk)
-    approvements = ApprovementService.get_approvements_object_waiting_for_approval(obj, [getattr(obj, state_field)])
+    waiting_approvements = ApprovementService.get_approvements_object_waiting_for_approval(obj, [getattr(obj, state_field)])
 
     ctx = context
-    ctx['approvements'] = approvements
+    ctx['approvements'] = waiting_approvements
     ctx['obj_pk'] = obj_pk
+    ctx['approve_button_text'] = approve_button_text
+    ctx['reject_button_text'] = reject_button_text
     return ctx
