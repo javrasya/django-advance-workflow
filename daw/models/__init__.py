57c86b5ec3743917b473a45901e107eca003225e
Ahmet DAL <ceahmetdal@gmail.com>
2014-06-26 16:05:33 +0300

    minor changes.

--- daw/models/__init__.py
+++ daw/models/__init__.py
@@ -2,6 +2,8 @@ from south.modelsinspector import add_introspection_rules
 
 __author__ = 'ahmetdal'
 
+from externalpermission import *
+from externalcontenttype import *
 from state import *
 from transition import *
 from transitionapprovedefinition import *
--- daw/models/transitionapprovedefinition.py
+++ daw/models/transitionapprovedefinition.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import Permission
 from django.db import models
+
 from django.utils.translation import ugettext_lazy as _
 
 from daw.models import Transition, BaseModel
-from daw.models.externalpermission import ExternalPermission
 from daw.models.managers.transitionapprovedefinitionmanager import TransitionApproveDefinitionManager
 
 
@@ -18,7 +19,7 @@ class TransitionApproveDefinition(BaseModel):
         unique_together = [('transition', 'permission'), ('transition', 'permission', 'order')]
 
     transition = models.ForeignKey(Transition, verbose_name=_('Transition'))
-    permission = models.ForeignKey(ExternalPermission, verbose_name=_('Permission'))
+    permission = models.ForeignKey(Permission, verbose_name=_('Permission'))
     order = models.IntegerField(default=0, verbose_name=_('Order'))
 
     objects = TransitionApproveDefinitionManager()
