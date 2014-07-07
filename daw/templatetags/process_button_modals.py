57ad6f5b628cf405f3c4a88de37c550bcdc26800
Ahmet DAL <ceahmetdal@gmail.com>
2014-07-07 13:59:48 +0300

    processor modals now is getting state field

--- daw/templatetags/process_button_modals.py
+++ daw/templatetags/process_button_modals.py
@@ -9,6 +9,7 @@ DAW_PROCESS_BUTTON_MODALS_TEMPLATE = 'process_button_modals.html'
 
 
 @register.inclusion_tag(DAW_PROCESS_BUTTON_MODALS_TEMPLATE, takes_context=True)
-def daw_process_button_modals(context, cls):
+def daw_process_button_modals(context, cls, state_field):
     context['cls'] = cls
+    context['state_field'] = state_field
     return context
