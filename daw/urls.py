dfbb96fb90142160435a2e60a06552442f45fc94
Ahmet DAL <ceahmetdal@gmail.com>
2014-09-05 14:52:26 +0300

    skip transition view is added.

--- daw/urls.py
+++ daw/urls.py
@@ -15,4 +15,8 @@ urlpatterns = patterns('daw.views',
                            r'^get_current_state/(?P<content_type_id>[$a-zA-Z0-9]+)/(?P<obj_pk>[$a-zA-Z0-9]+)/(?P<state_field>[$a-zA-Z0-9_]+)/?$', 'get_current_state'),
                        url(
                            r'^get_state_by_label/(?P<label>[$a-zA-Z0-9]+)/?$', 'get_state_by_label'),
+                       url(
+                           r'^skip_transition/(?P<content_type_id>[$a-zA-Z0-9]+)/(?P<object_id>[$a-zA-Z0-9]+)/(?P<state_field>[$a-zA-Z0-9_]+)/(?P<destination_state_id>[$a-zA-Z0-9]+)/?$',
+                           'skip_transition'),
+
 )
--- daw/views.py
+++ daw/views.py
@@ -5,6 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest
 from daw.models import State
+from daw.models.transitionapprovement import TransitionApprovement
 
 from daw.service.approvementservice import ApprovementService
 from daw.service.transitionservice import TransitionService
@@ -46,6 +47,17 @@ def get_current_state(request, content_type_id, obj_pk, state_field):
 def get_state_by_label(request, label):
     state = State.objects.get(label=label)
     return HttpResponse(json.dumps({'id': state.pk, 'label': state.label}), content_type='application/json')
+
+
+def skip_transition(request, content_type_id, object_id, state_field, destination_state_id):
+    try:
+        state = TransitionApprovement.objects.filter(
+            content_type__pk=content_type_id,
+            object_pk=object_id,
+            approve_definition__transition__destination_state__pk=destination_state_id).update(skip=True)
+        return HttpResponse(json.dumps({'id': state.pk, 'label': state.label}), content_type='application/json')
+    except Exception, e:
+        return HttpResponseBadRequest(json.dumps({'error_message': e.message}), content_type='application/json')
 
 
 def _get_decoded_callback_url(url):
